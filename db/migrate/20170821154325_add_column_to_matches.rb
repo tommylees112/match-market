7236418cec66d1a60b3c4fc566630a18408c8d24
Tommy Lees <thomas.lees112@gmail.com>
2017-08-21 16:59:16 +0100

    added the commas in hte migration file

--- db/migrate/20170821154325_add_column_to_matches.rb
+++ db/migrate/20170821154325_add_column_to_matches.rb
@@ -1,6 +1,6 @@
 class AddColumnToMatches < ActiveRecord::Migration[5.0]
   def change
-    add_column :matches, :home_logo :string
-    add_column :matches, :away_logo :string
+    add_column :matches, :home_logo, :string
+    add_column :matches, :away_logo, :string
   end
 end
