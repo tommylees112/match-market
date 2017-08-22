de7653870c3932efc3dd3bca8bc02299b208bc7b
Hamish Hall <hachall@hotmail.com>
2017-08-22 15:54:16 +0100

    league tabs filter cards by league

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,11 @@
 class MatchesController < ApplicationController
   def index
     @matches = Match.all
+    @premiership_matches = Match.where(league: "Premier League 2017/18")
+    @bundesliga_matches = Match.where(league: "Bundesliga 2017/18")
+    @laliga_matches = Match.where(league: "La Liga 2017/18")
+    @ligue1_matches = Match.where(league: "Ligue 1 2017/18")
+    @serieA_matches = Match.where(league: "Serie A 2017/18")
   end
 
   def show
