3daf085f583e0fa733c0d205e57b7f13ed09a502
Tommy Lees <thomas.lees112@gmail.com>
2017-08-22 16:50:20 +0100

    seeding the odds for each match and implementing a simple card

--- db/_odds_seed.rb
+++ db/_odds_seed.rb
@@ -17,15 +17,15 @@ def generate_odd_params(odd)
   odd.save
 end
 
-Match.first(1).each do |match|
-  OUTCOME.each do |outcome|
-    5.times do
-      odd = Odd.new(match: match)
-      odd.outcome = outcome
-      odds_number = rand(1.1..2.0).round(2)
-      odd.odds = odds_number
-      odd.user = User.find(1)
-      odd.save
-    end
-  end
-end
+# Match.all.each do |match|
+#   OUTCOME.each do |outcome|
+#     5.times do
+#       odd = Odd.new(match: match)
+#       odd.outcome = outcome
+#       odds_number = rand(1.1..2.0).round(2)
+#       odd.odds = odds_number
+#       odd.user = User.find(1)
+#       odd.save
+#     end
+#   end
+# end
