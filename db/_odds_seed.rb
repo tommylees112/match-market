6c73b3c9dd77ac25fec9336887e445a046a83698
Tommy Lees <thomas.lees112@gmail.com>
2017-08-23 16:58:44 +0100

    create a heroku console compatible odds seed method

--- db/_odds_seed.rb
+++ db/_odds_seed.rb
@@ -18,16 +18,51 @@ def generate_odd_params(odd)
 end
 
 ###### CONSOLE METHOD
+tommy = User.find(1) #_by_email("thomas.lees@chch.ox.ac.uk")
+hamish = User.find(3) #_by_email("hachall@hotmail.com")
+adrian = User.find(2) #_by_email("walkerrrrrrr@gmail.com")
+USER = [tommy, hamish, adrian]
+OUTCOME = ["Home", "Away", "Draw"]
 
 Match.all.each do |match|
   OUTCOME.each do |outcome|
-    5.times do
+    3.times do
       odd = Odd.new(match: match)
       odd.outcome = outcome
       odds_number = rand(1.1..2.0).round(2)
       odd.odds = odds_number
-      odd.user = User.find(1)
-      odd.save
+      odd.user = USER.sample
+      odd.save!
     end
   end
 end
+
+## RAKFILE $ rails odds:seed
+namespace :odds do
+  task seed: :environment do
+    OUTCOME = ["Home", "Away", "Draw"]
+    tommy = User.find(1) #_by_email("thomas.lees@chch.ox.ac.uk")
+    hamish = User.find(3) #_by_email("hachall@hotmail.com")
+    adrian = User.find(2) #_by_email("walkerrrrrrr@gmail.com")
+    USER = [tommy, hamish, adrian]
+
+    def generate_odd_params(odd)
+      odd.user = USER.sample
+      odds_number = rand(1.1..2.0).round(2)
+      odd.odds = odds_number
+      odd.save!
+    end
+
+    Match.all.each do |match|
+      OUTCOME.each do |outcome|
+        3.times do
+          odd = Odd.new(match: match)
+          odd.outcome = outcome
+          generate_odd_params(odd)
+        end
+      end
+    end
+
+  end
+end
+
