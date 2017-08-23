658df068052b8f8ed0458dc4d5eda664250708ee
Tommy Lees <thomas.lees112@gmail.com>
2017-08-23 12:18:54 +0100

    continued to work on the dashboard creating the user info card

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def dashboard
     @user = current_user
     @created_odds = @user.odds
-    # @booked_odds = @user.bookings
+    @user_bookings = @user.bookings
     # owned_odds = how distinguish from bought odds?
   end
 end
