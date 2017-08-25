277f1eb4eeb3dac0d03bd4bd339bd9e2fa1b800d
Hamish Hall <hachall@hotmail.com>
2017-08-25 13:58:03 +0100

    v v nealy finished full comment board sidebare, just needs scrollable content

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,22 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
-    @comment.match = Match.find(params[:match_id])
+    @match = Match.find(params[:match_id])
+    @comment.match = @match
     @comment.user = current_user
-    @comment.save
+
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to match_path(@match) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render 'matches/show' }
+        format.js  # <-- idem
+      end
+    end
+
   end
 
   private
