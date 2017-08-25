0c08865ddffc0c55becda4a5f3951df82818cf35
Tommy Lees <thomas.lees112@gmail.com>
2017-08-25 15:20:34 +0100

    adding a filter to the dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
       @user_bookings = @user.bookings.joins(:odd).joins(:match).where(won: false).order('matches.match_date DESC')
     when 'pending'
       @user_bookings = @user.bookings.joins(:odd).joins(:match).where(won: nil).order('matches.match_date DESC')
+    when 'all'
+      @user_bookings = @user.bookings.joins(:odd).joins(:match).order('matches.match_date DESC')
     else
       @user_bookings = @user.bookings.joins(:odd).joins(:match).order('matches.match_date DESC')
     end
