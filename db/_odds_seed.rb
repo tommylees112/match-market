87b20273a46797104b722bf25b2a0fb15a9edc32
Tommy Lees <thomas.lees112@gmail.com>
2017-08-25 16:49:11 +0100

    FUCKING CRAY CHANGES DEMO IN 5

--- db/_odds_seed.rb
+++ db/_odds_seed.rb
@@ -21,17 +21,24 @@ end
 tommy = User.find(1) #_by_email("thomas.lees@chch.ox.ac.uk")
 hamish = User.find(3) #_by_email("hachall@hotmail.com")
 adrian = User.find(2) #_by_email("walkerrrrrrr@gmail.com")
-USER = User.find(1) #[tommy, hamish, adrian]
+
+
+user_1 = User.find(21) #[tommy, hamish, adrian]
+user_2 = User.find(19) #[tommy, hamish, adrian]
+user_3 = User.find(24) #[tommy, hamish, adrian]
+user_4 = User.find(25) #[tommy, hamish, adrian]
+
+USER = [user_1, user_2, user_3, user_4]
 OUTCOME = ["Home", "Away", "Draw"]
 
 Match.all.each do |match|
   OUTCOME.each do |outcome|
-    1.times do
+    2.times do
       odd = Odd.new(match: match)
       odd.outcome = outcome
       odds_number = rand(1.1..2.0).round(2)
       odd.odds = odds_number
-      odd.user = USER
+      odd.user = USER.sample
       odd.max_people = rand(1..20)
       odd.save!
     end
@@ -43,7 +50,10 @@ USERS.sample
 ODDS = Odd.all
 
 ODDS.each do |odd|
+  booking = booking.new
+
 end
+
 
 
 
